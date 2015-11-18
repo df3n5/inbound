7337342a619d8bfe30f111f903096b8e0d75a604
Jonathan Frawley <jmf@9lines.org>
2015-11-18 23:44:10 +0000

    Flags

--- src/main.c
+++ src/main.c
@@ -4,7 +4,13 @@
 #include "cog/src/cog_anim.h"
 #include "cog/src/cog_math.h"
 
-static const int N_ENTITIES = 1;
+static const double X_SPEED = 0.3;
+static const double X_SPEED_AIR = 0.5;
+static const double X_FRICTION = 0.002;
+
+static const double JUMP_SPEED = 0.6;
+static const uint32_t JUMP_MILLIS = 750;
+static const double GRAVITY = -0.6;
 
 typedef struct player {
     cog_sprite* s;
@@ -28,22 +34,40 @@ void update() {
 }
 
 int main(int argc, char* argv[]) {
-    cog_init(.fullscreen = false);
+    cog_init(.fullscreen = false, .window_w=600, .window_h=600);
+		int jumping = 0;
 		int moving = 0;
+		uint32_t jump_time = 0;
 
 		init();
     while(!cog_hasquit()) {
 				update();
         cog_loopstep();
+				if(jumping) {
+						jump_time += cog_time_delta_millis();
+						if(jump_time > JUMP_MILLIS) {
+							jumping = 0;
+							p.s->vel.y = GRAVITY;							
+						}
+				} else {
+						if(p.s->pos.y < 0.0) p.s->vel.y = 0.0;
+				}
+
         if(cog_input_key_pressed()) {
             uint32_t key = cog_input_key_code_pressed();
-            cog_debugf("key is %d", key);
+            if(key == 'w') {
+							p.s->vel.y = JUMP_SPEED;
+							jumping = 1;
+							jump_time = 0;
+							if(p.s->vel.x > 0.001) p.s->vel.x = X_SPEED_AIR;
+							if(p.s->vel.x < -0.001) p.s->vel.x = -X_SPEED_AIR;
+            }
             if(key == 'a') {
-							p.s->vel.x = -0.1;							
+							p.s->vel.x = jumping ? -X_SPEED_AIR : -X_SPEED;
 							moving = 1;
             }
             if(key == 'd') {
-							p.s->vel.x = 0.1;
+							p.s->vel.x = jumping ? X_SPEED_AIR : X_SPEED;
 							moving = 1;
             }
         } 
@@ -55,8 +79,8 @@ int main(int argc, char* argv[]) {
 				}
 				if(!moving) {
 						if(fabs(p.s->vel.x) > 0.01) {
-								if(p.s->vel.x > 0.0) p.s->vel.x -= 0.002;
-								else p.s->vel.x += 0.002;
+								if(p.s->vel.x > 0.0) p.s->vel.x -= X_FRICTION;
+								else p.s->vel.x += X_FRICTION;
 						} else {
 								p.s->vel.x = 0.0;
 						}
