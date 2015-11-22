69927146c73f608256ea5c112a461042598d0953
Jonathan Frawley <jmf@9lines.org>
2015-11-22 15:35:20 +0000

    Some small improvements

--- src/main.c
+++ src/main.c
@@ -4,9 +4,9 @@
 #include "cog/src/cog_anim.h"
 #include "cog/src/cog_math.h"
 
-static const double X_SPEED = 0.3;
-static const double X_SPEED_AIR = 0.5;
-static const double X_FRICTION = 0.002;
+static const double X_SPEED = 0.5;
+static const double X_SPEED_AIR = 0.7;
+static const double X_FRICTION = 0.0002;
 
 static const double JUMP_SPEED = 0.6;
 static const uint32_t JUMP_MILLIS = 750;
@@ -34,9 +34,9 @@ void update() {
 }
 
 int main(int argc, char* argv[]) {
-    cog_init(.fullscreen = false, .window_w=600, .window_h=600);
-		int jumping = 0;
-		int moving = 0;
+    cog_init(.fullscreen = false, .debug=false, .window_w=600, .window_h=600);
+		bool jumping = false;
+		bool moving = false;
 		uint32_t jump_time = 0;
 
 		init();
@@ -44,37 +44,42 @@ int main(int argc, char* argv[]) {
 				update();
         cog_loopstep();
 				if(jumping) {
+						// Jump lasts an amount of time.
 						jump_time += cog_time_delta_millis();
 						if(jump_time > JUMP_MILLIS) {
-							jumping = 0;
 							p.s->vel.y = GRAVITY;							
+							// Collide with ground
+							if(p.s->pos.y < 0.0) {
+									jumping = false;
+									p.s->vel.y = 0.0;
+									cog_debugf("hi");
+							}
 						}
-				} else {
-						if(p.s->pos.y < 0.0) p.s->vel.y = 0.0;
 				}
 
         if(cog_input_key_pressed()) {
             uint32_t key = cog_input_key_code_pressed();
-            if(key == 'w') {
+						cog_debugf("Key w <%d> and jumping <%d>", key=='w', jumping);
+            if(!jumping && key == 'w') {
 							p.s->vel.y = JUMP_SPEED;
-							jumping = 1;
+							jumping = true;
 							jump_time = 0;
 							if(p.s->vel.x > 0.001) p.s->vel.x = X_SPEED_AIR;
 							if(p.s->vel.x < -0.001) p.s->vel.x = -X_SPEED_AIR;
             }
             if(key == 'a') {
 							p.s->vel.x = jumping ? -X_SPEED_AIR : -X_SPEED;
-							moving = 1;
+							moving = true;
             }
             if(key == 'd') {
 							p.s->vel.x = jumping ? X_SPEED_AIR : X_SPEED;
-							moving = 1;
+							moving = true;
             }
         } 
 				if(cog_input_key_depressed()) {
 						uint32_t key = cog_input_key_code_depressed();
 						if(key == 'a' || key == 'd') {
-								moving = 0;
+								moving = false;
 						}
 				}
 				if(!moving) {
