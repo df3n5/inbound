864d8d67b1a9fff4dcf21cac60cc00896af2dcef
Jonathan Frawley <jmf@9lines.org>
2015-11-26 00:14:35 +0000

    First level

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <SDL2/SDL_opengl.h>
 
 #include "cog/src/cog.h"
 #include "cog/src/cog_anim.h"
@@ -13,6 +14,8 @@ static double PLATFORM_HEIGHT = 0.05;
 static const double JUMP_SPEED = 0.6;
 static const uint32_t JUMP_MILLIS = 750;
 static const double GRAVITY = -0.6;
+
+static cog_pos2 START_POS = (cog_pos2) { .x=-0.9, .y=-0.35 };
 
 typedef struct player {
     //cog_sprite* s;
@@ -43,9 +46,7 @@ void init() {
     // Player
     p.sid = cog_rect_add();
     cog_rect_set(p.sid, (cog_rect) {
-            .pos=(cog_pos2) {
-            .x=-0.9, .y=-0.35
-            },
+            .pos=START_POS,
             .dim=(cog_dim2) {
             .w=0.05, .h=0.1
             },
@@ -122,6 +123,24 @@ void init() {
             .sid=pl_1
             });
 
+    cog_rect_id pl_2 = cog_rect_add();
+    cog_rect_set(pl_2, (cog_rect) {
+            .dim=(cog_dim2) {
+            .w=0.1, .h=PLATFORM_HEIGHT
+            },
+            .rot=COG_PI/2,
+            .pos=(cog_pos2) {
+            .x=0.9, .y=-0.5
+            },
+            .col=(cog_color) {
+            .r=1,.g=1,.b=1,.a=1
+            }
+            });
+    cog_list_append(&platforms, &(platform) {
+            .sid=pl_2
+            });
+
+
     cog_rect* pl_0_r = cog_rect_get(pl_0);
     cog_rect* pl_1_r = cog_rect_get(pl_1);
 
@@ -131,7 +150,7 @@ void init() {
     cog_rect_id trap_0 = cog_rect_add();
     cog_rect_set(trap_0, (cog_rect) {
             .dim=(cog_dim2) {
-            .w=0.35, .h=PLATFORM_HEIGHT*6
+            .w=0.35, .h=PLATFORM_HEIGHT*2.0
             },
             .rot=COG_PI/2,
             .pos=(cog_pos2) {
@@ -141,13 +160,40 @@ void init() {
             .r=1,.g=0,.b=0,.a=1
             }
             });
-
     cog_list_append(&traps, &(trap) {
-            .sid=pl_0
+            .sid=trap_0
+            });
+
+    cog_rect_id trap_1 = cog_rect_add();
+    cog_rect_set(trap_1, (cog_rect) {
+            .dim=(cog_dim2) {
+            .w=0.20, .h=PLATFORM_HEIGHT*5.0
+            },
+            .rot=COG_PI/2,
+            .pos=(cog_pos2) {
+            .x=(pl_1_r->pos.x) + 0.4, .y=-0.5
+            },
+            .col=(cog_color) {
+            .r=1,.g=0,.b=0,.a=1
+            }
+            });
+    cog_list_append(&traps, &(trap) {
+            .sid=trap_1
             });
 }
 
 void update() {
+}
+
+void render() {
+    COG_LIST_FOREACH(&traps) {
+        trap* t = (trap*)curr->data;
+        cog_rect* trap_rect = cog_rect_get(t->sid);
+        glBegin(GL_LINES);
+            glVertex2f(trap_rect->pos.x,trap_rect->pos.y);
+            glVertex2f(p.s->pos.x,p.s->pos.y);
+        glEnd();
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -157,6 +203,8 @@ int main(int argc, char* argv[]) {
     uint32_t jump_time = 0;
 
     init();
+
+    cog_graphics_set_custom_render(render);
     while(!cog_hasquit()) {
         update();
         cog_loopstep();
@@ -211,7 +259,13 @@ int main(int argc, char* argv[]) {
             trap* t = (trap*)curr->data;
             if(cog_rect_collides_rect(t->sid, p.sid)) {
                 cog_debugf("COLLISON");
+                p.s->pos = START_POS; 
             }
+        }
+        // Win condition
+        if(p.s->pos.x > 1.0) {
+            //cog_clear();
+            cog_debugf("Finished level. TODO: Add more levels.");
         }
     }
 }
